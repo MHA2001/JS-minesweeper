d4c0dd282bdeeaf91895c5f75b0c038c1892ba8c
MHA2001 <mha.akhoudndi@gmail.com>
2025-08-07 16:00:38 +0330

    feat: adding game logic and functionality:
    - revealing tiles and
    - check wining and losing

--- minesweeper.js
+++ minesweeper.js
@@ -1,6 +1,6 @@
 // logic
 
-const TILE_STATUSES = {
+export const TILE_STATUSES = {
 	HIDDEN: 'hidden',
 	MINE: 'mine',
 	NUMBER: 'number',
@@ -51,6 +51,59 @@ export function markTile(tile) {
 	} else {
 		tile.status = TILE_STATUSES.MARKED;
 	}
+}
+
+export function revealTile(board, tile) {
+	if (tile.status !== TILE_STATUSES.HIDDEN) {
+		return;
+	}
+
+	if (tile.mine) {
+		tile.status = TILE_STATUSES.MINE;
+		return;
+	}
+	tile.status = TILE_STATUSES.NUMBER;
+	const adjacentTiles = nearbyTiles(board, tile);
+	const mines = adjacentTiles.filter((t) => t.mine);
+	if (mines.length === 0) {
+		adjacentTiles.forEach(revealTile.bind(null, board));
+	} else {
+		tile.element.textContent = mines.length;
+	}
+}
+
+export function checkWin(board) {
+	return board.every((row) => {
+		return row.every((tile) => {
+			return (
+				tile.status === TILE_STATUSES.NUMBER ||
+				(tile.mine &&
+					(tile.status === TILE_STATUSES.HIDDEN ||
+						tile.status === TILE_STATUSES.MARKED))
+			);
+		});
+	});
+}
+
+export function checkLose(board) {
+	return board.some((row) => {
+		return row.some((tile) => {
+			return tile.status === TILE_STATUSES.MINE;
+		});
+	});
+}
+
+function nearbyTiles(board, { x, y }) {
+	const tiles = [];
+
+	for (let xOffset = -1; xOffset <= 1; xOffset++) {
+		for (let yOffset = -1; yOffset <= 1; yOffset++) {
+			const tile = board[x + xOffset]?.[y + yOffset];
+			if (tile) tiles.push(tile);
+		}
+	}
+
+	return tiles;
 }
 
 function getMinePositions(size, numberOfMines) {
